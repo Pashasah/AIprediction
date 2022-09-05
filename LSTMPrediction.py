9d62aa86a53b144444a643d8e93f6d7ffd53b66c
Shahab Pasha <shahab.pasha@rejlers.se>
2022-09-05 10:54:40 +0200

    Signed-off-by: Shahab Pasha <shahab.pasha@rejlers.se>

--- LSTMPrediction.py
+++ LSTMPrediction.py
@@ -19,8 +19,8 @@ from sklearn.preprocessing import MinMaxScaler
 
 import pandas as pd
 
-TrL=1000
-
+TrL=1200
+ARO=120
 df = pd.read_csv('HD.csv')
 
 df = df[['Date', 'Close']]
@@ -51,8 +51,8 @@ valid_data=final_data[TrL:,:]
 scaler=MinMaxScaler(feature_range=(0,1))
 scaled_data=scaler.fit_transform(final_data)
 x_train_data,y_train_data=[],[]
-for i in range(60,len(train_data)):
-    x_train_data.append(scaled_data[i-60:i,0])
+for i in range(ARO,len(train_data)):
+    x_train_data.append(scaled_data[i-ARO:i,0])
     y_train_data.append(scaled_data[i,0])
     
     
@@ -60,7 +60,7 @@ lstm_model=Sequential()
 lstm_model.add(LSTM(units=90,return_sequences=True,input_shape=(np.shape(x_train_data)[1],1)))
 lstm_model.add(LSTM(units=90))
 lstm_model.add(Dense(1))
-model_data=data[len(data)-len(valid_data)-60:].values
+model_data=data[len(data)-len(valid_data)-ARO:].values
 model_data=model_data.reshape(-1,1)
 model_data=scaler.transform(model_data)
 
@@ -70,8 +70,8 @@ y_train_data = np.asarray(y_train_data)
 lstm_model.compile(loss='mean_squared_error',optimizer='adam')
 lstm_model.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2)
 X_test=[]
-for i in range(60,model_data.shape[0]):
-    X_test.append(model_data[i-60:i,0])
+for i in range(ARO,model_data.shape[0]):
+    X_test.append(model_data[i-ARO:i,0])
 X_test=np.array(X_test)
 X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
 
